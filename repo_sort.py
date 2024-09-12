6e49d760e8e58453931d9b26cf013b86962d00ef
Sean Currlin <sean.currlin@gmail.com>
2024-09-11 22:21:32 -0400

    more formatting changes

--- repo_sort.py
+++ repo_sort.py
@@ -59,7 +59,7 @@ I have a demonstrated proficiency in software development, with a proven track r
 
 ![My Skills](https://skillicons.dev/icons?i=js,react,express,mongodb,nodejs,nextjs,threejs,tailwind,python,django,flask,postgres,postman,vercel,git)
 
-|               |               |
+| JavaScript    | Python        |
 | ------------- | ------------- |
 | JavaScript    | Python        |
 | React         | Django        |
@@ -100,7 +100,7 @@ for page_num in range(total_pages):
 
         # Add the repository to the README content
         readme_content += f"### [{repo['name']}]({repo['html_url']})\n"
-        readme_content += f"{language_color} {language} Created on {formatted_date}\n\n"
+        readme_content += f"{language_color} {language} • Created on {formatted_date}\n\n"
 
 # Write the generated content to the README.md file
 with open("README.md", "w") as readme_file:
