c76c247474d62adbae3dc9b1e581c904a7f3c6cc
Sean Currlin <sean.currlin@gmail.com>
2024-09-12 08:41:57 -0400

    fork test 12

--- repo_sort.py
+++ repo_sort.py
@@ -112,7 +112,8 @@ for page_num in range(total_pages):
 
         # Add the repository to the README content
         readme_content += f"### [{repo['name']}]({repo['html_url']})\n"
-        readme_content += f"{language_color} {language} • Created on {formatted_date} {fork_info}\n\n"
+        readme_content += f"{language_color} {language} • Created on {formatted_date}\n"
+        readme_content += f"{fork_info}\n"
 
         # Omit separator if it's the last repository on the page
         if index < len(page_repos) - 1:
