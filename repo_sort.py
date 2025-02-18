37854ca45120d78242a61e69a3543a1f92242c2f
Sean Currlin <sean.currlin@gmail.com>
2025-02-18 10:59:37 -0500

    update readme content

--- repo_sort.py
+++ repo_sort.py
@@ -56,7 +56,7 @@ readme_content = """
 <table>
 <tr>
 <td>
-I have a demonstrated proficiency in software development, with a proven track record of delivering high-quality solutions from ideation to deployment...
+I have a demonstrated proficiency in software development, with a proven track record of delivering high-quality solutions from ideation to deployment. My expertise includes developing and optimizing web applications, enhancing data pipelines, and implementing analytics to generate actionable insights. I possess a deep understanding of full-stack development, particularly in Python, JavaScript, React.js, and database management. My polished communication skills enable me to effectively articulate complex technical strategies.
 </td>
 </tr>
 </table>
